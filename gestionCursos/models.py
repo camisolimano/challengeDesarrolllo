a04a2504f20b29c63673f88bc8cbc1b389cc28c3
camisolimano <csolimano@frba.utn.edu.ar>
2024-12-20 21:35:42 -0300

    base challenge, conectado con postgresql

---
+++ gestionCursos/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Alumno(models.Model):
+    nombre=models.CharField( max_length=50)
+    apellido=models.CharField( max_length=50)
+    fecha_nac=models.DateField()
+    dni=models.CharField(max_length=20, unique=True)
+    direccion=models.CharField(max_length=30)
+
+class Sede(models.Model):
+    nombre=models.CharField()
+    direccion=models.CharField()
+    ciudad=models.CharField()
+    telefono=models.CharField()
+    cant_aulas=models.IntegerField()
+
+class Curso(models.Model):
+    año_dictado=models.IntegerField()
+    duracion=models.IntegerField()
+    tema=models.CharField()
+    costo_mensual=models.DecimalField(max_digits=20, decimal_places=3)
+    fecha_inicio=models.DateField()
+    fecha_fin=models.DateField()
+    sede= models.ForeignKey(Sede, on_delete=models.CASCADE)#un curso se dicta en una sede, pero en una sede hay muchos cursos
+    alumnos=models.ManyToManyField(Alumno)
