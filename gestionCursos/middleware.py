2e0316d3d9f5843c65d04bc3ab1e4e9cb913d134
Camila Solimano <casolimano@dblandit.com>
2025-12-24 12:08:01 -0300

    Deploy

--- gestionCursos/middleware.py
+++ gestionCursos/middleware.py
@@ -35,14 +35,4 @@ class PostHogExceptionMiddleware(MiddlewareMixin):
         
         return None
     
-    
-class PostHogCSPMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response["Content-Security-Policy"] = (
-            "frame-ancestors 'self' https://us.posthog.com"
-        )
-        return response
+ 
