2f414475ba5bd5d43b313f5c4143edbd379a9afc
Camila Solimano <casolimano@dblandit.com>
2025-12-26 12:25:57 -0300

    borro middleware

--- challenge/settings.py
+++ challenge/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'gestionCursos.middleware.PostHogExceptionMiddleware',
-    'gestionCursos.middleware.PostHogCSPMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware', 
 
 
