db141120bdef551182d0630c3789f167828f28f8
Camila Solimano <casolimano@dblandit.com>
2026-01-05 14:48:17 -0300

    Agrego eventos

--- gestionCursos/posthog_client.py
+++ gestionCursos/posthog_client.py
@@ -4,7 +4,7 @@ from django.conf import settings
 posthog_client = Posthog(
     project_api_key=settings.POSTHOG_API_KEY,
     host=settings.POSTHOG_HOST,
-    enable_exception_autocapture=True
+    enable_exception_autocapture= False
 )
 
 def capture(user, event_name: str, properties: dict | None = None) -> None:
